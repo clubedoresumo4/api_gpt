4b388d595fd5619b0ddcb37d1f2f6c08b1479f25
clubedoresumo4 <clubedoresumo4@gmail.com>
2024-05-25 05:47:07 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -19,7 +19,7 @@ def get_answer():
     try:
         client = Client()
         response = client.chat.completions.create(
-            model="gpt4",
+            model="gpt-4",
             messages=[{"role": "user", "content": f'''{question}'''}],
         )    
         return jsonify({'resposta': response.choices[0].message.content})
