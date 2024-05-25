092336e2170a503f606057f8a4e85fbde9a47a80
clubedoresumo4 <clubedoresumo4@gmail.com>
2024-05-25 05:34:11 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,11 +4,7 @@ from g4f.cookies import set_cookies
 app = Flask(__name__)
 
 
-set_cookies(".google.com", {
-  "__Secure-1PSIDCC": "AKEyXzUPG7_1fAQjMLNE_PMoVqhfTswE53e_p0ibIJeFFyvjpd9iAGj1PtvilZqjKe3f4_sm0Q",
-  "__Secure-1PSIDTS": "sidts-CjEBLwcBXDGRKlxKI6aFGQG5DvxSOAOYomOSXSQ6OI6HbRh1c3SzPFG2UyZDdu9RWa4AEAA",
-  "__Secure-1PSID":"g.a000kAikKfidrFdIUqxvSZqS8p_jUkiEp3tgXBea-ObFbHY45LRdMfzaxJyu4v5-jTH1XhyqygACgYKATASARYSFQHGX2Miq87l-Z8ddHn1HvDg_vZNYRoVAUF8yKoB_OqyY_K9n3kG3ro4EMcO0076"
-})
+
 @app.route('/')
 def home():
     return 'Hello, World!'
@@ -19,6 +15,11 @@ def get_answer():
     if not question:
         return jsonify({'error': 'Question parameter is required'}), 400    
     try:
+        set_cookies(".google.com", {
+          "__Secure-1PSIDCC": "AKEyXzUPG7_1fAQjMLNE_PMoVqhfTswE53e_p0ibIJeFFyvjpd9iAGj1PtvilZqjKe3f4_sm0Q",
+          "__Secure-1PSIDTS": "sidts-CjEBLwcBXDGRKlxKI6aFGQG5DvxSOAOYomOSXSQ6OI6HbRh1c3SzPFG2UyZDdu9RWa4AEAA",
+          "__Secure-1PSID":"g.a000kAikKfidrFdIUqxvSZqS8p_jUkiEp3tgXBea-ObFbHY45LRdMfzaxJyu4v5-jTH1XhyqygACgYKATASARYSFQHGX2Miq87l-Z8ddHn1HvDg_vZNYRoVAUF8yKoB_OqyY_K9n3kG3ro4EMcO0076"
+        })
         client = Client()
         response = client.chat.completions.create(
             model="gemini",
