50d18ede998efb719f020607cb461f051405eefa
clubedoresumo4 <clubedoresumo4@gmail.com>
2024-05-25 05:27:15 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
 from g4f.client import Client
-
+from g4f.cookies import set_cookies
 app = Flask(__name__)
 
+
+set_cookies(".google.com", {
+  "__Secure-1PSIDCC": "AKEyXzUPG7_1fAQjMLNE_PMoVqhfTswE53e_p0ibIJeFFyvjpd9iAGj1PtvilZqjKe3f4_sm0Q",
+  "__Secure-1PSIDTS": "sidts-CjEBLwcBXDGRKlxKI6aFGQG5DvxSOAOYomOSXSQ6OI6HbRh1c3SzPFG2UyZDdu9RWa4AEAA",
+  "__Secure-1PSID":"g.a000kAikKfidrFdIUqxvSZqS8p_jUkiEp3tgXBea-ObFbHY45LRdMfzaxJyu4v5-jTH1XhyqygACgYKATASARYSFQHGX2Miq87l-Z8ddHn1HvDg_vZNYRoVAUF8yKoB_OqyY_K9n3kG3ro4EMcO0076"
+})
 @app.route('/')
 def home():
     return 'Hello, World!'
